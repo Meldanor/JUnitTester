6ae0e62efd4d8e41457d50b306de7e38b42c1096
Kilian Gaertner <Bhaals@gmx.de>
2014-02-14 20:10:29 +0100

    Initial commit

---
+++ src/test/java/ClassFileReaderTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class ClassFileReaderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		CompleteFileReader cReader = new CompleteFileReader();
+	}
+
+}
