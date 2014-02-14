9e0931b9fafdca5e9e368489e21af8bbf5daf606
Kilian Gaertner <Bhaals@gmx.de>
2014-02-14 21:13:23 +0100

    Bla, markus ist schuld

--- src/main/java/CacheCompiler.java
+++ src/main/java/CacheCompiler.java
@@ -1,3 +1,95 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
+
 public class CacheCompiler {
 
+    private JavaCompiler compiler;
+    private CompleteFileReader cReader;
+
+    public CacheCompiler() {
+        this.compiler = ToolProvider.getSystemJavaCompiler();
+        // True, when JDK not installed or javac not linked
+        if (this.compiler == null) {
+            throw new RuntimeException("Can't find java compiler. Is JDK installed?");
+        }
+
+        cReader = new CompleteFileReader();
+    }
+
+    public Class<?> loadClass(String className, String classContent) {
+        List<JavaObject> compileObjects = Arrays.asList(new JavaObject(className, classContent));
+
+        // Collector for compilation information
+        DiagnosticCollector<Object> dia = new DiagnosticCollector<Object>();
+        // Create task to compile
+        CompilationTask task = compiler.getTask(null, null, dia, null, null, compileObjects);
+        // Start compiling
+        boolean result = task.call();
+
+        // Print out results - if compilation was successfull, nothing happens
+        List<Diagnostic<? extends Object>> diagnostics = dia.getDiagnostics();
+        StringBuilder sBuilder = new StringBuilder();
+        for (Diagnostic<? extends Object> diagnostic : diagnostics) {
+            sBuilder.append(diagnostic);
+            sBuilder.append("\n");
+        }
+
+        if (result) {
+            try {
+                className = className.substring(className.lastIndexOf('/') + 1);
+                className = className.replaceAll(".java", "");
+                return Class.forName(className);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        else {
+            System.out.println(sBuilder);
+
+            return null;
+        }
+    }
+
+    public Class<?> loadClass(String fileName) throws FileNotFoundException {
+        String content = cReader.readFile(fileName);
+
+        return loadClass(fileName, content);
+    }
+
+    public Class<?> loadClass(File file) throws FileNotFoundException {
+        return loadClass(file.getName());
+    }
+
+    /**
+     * Wrapper class for compiable objects created just be strings without an
+     * existing file source
+     */
+    private class JavaObject extends SimpleJavaFileObject {
+
+        private String content;
+
+        public JavaObject(String className, String content) {
+            super(URI.create(className), Kind.SOURCE);
+            this.content = content;
+        }
+
+        @Override
+        public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+            return content;
+        }
+
+    }
 }
--- src/test/java/ClassFileReaderTest.java
+++ src/test/java/ClassFileReaderTest.java
@@ -2,6 +2,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class ClassFileReaderTest {
@@ -15,6 +19,20 @@ public class ClassFileReaderTest {
         assertNotNull(s);
         assertFalse(s.isEmpty());
         assertTrue(s.contains("public class Counter"));
+    }
+
+    @Test
+    public void testa() throws FileNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+        CacheCompiler c = new CacheCompiler();
+        Class<?> clazz = c.loadClass("src/test/resources/Counter.java");
+        Object o = clazz.newInstance();
+        Method m = clazz.getMethod("count");
+        m.invoke(m);
+
+        Method m2 = clazz.getMethod("toString");
+        String s = (String) m2.invoke(o);
+        System.out.println(s);
+//        clazz.getMethod(name, parameterTypes)
     }
 
 }
